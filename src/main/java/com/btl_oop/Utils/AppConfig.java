892e14ddcc1d1edaad66e1f329a1490b15f48e93
LinhThaoPham <phamlinhthaonk@gmail.com>
2025-10-30 10:49:00 +0700

    update media

--- src/main/java/com/btl_oop/Utils/AppConfig.java
+++ src/main/java/com/btl_oop/Utils/AppConfig.java
@@ -10,7 +10,7 @@ public class AppConfig {
     public static String PATH_ORDER_MENU_SCREEN = "/com/btl_oop/FXML/Order/choose-dishes.fxml";
     public static String PATH_DISH_ITEM_PANEL = "/com/btl_oop/FXML/Order/dish-item.fxml";
     public static String PATH_ORDER_CARD = "/com/btl_oop/FXML/Kitchen/order-card.fxml";
-    
+    public static String PATH_NOTIFICATION_SOUND= "/com/btl_oop/Music/Order-up-bell-sound.mp3";
     public static String PATH_TABLE_MAP = "/com/btl_oop/FXML/DeskManager/TableMap.fxml";
     public static String PATH_ORDER_DETAILS = "/com/btl_oop/FXML/DeskManager/OrderDetails.fxml";
     public static String PATH_PAYMENT_PROCESSING = "/com/btl_oop/FXML/DeskManager/PaymentProcessing.fxml";
