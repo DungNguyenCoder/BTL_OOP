bc41ecc7b4a4a5a365765b58ec3f0502584fb706
DungNguyenCoder <nm.dung.it@gmail.com>
2025-11-03 14:27:01 +0700

    Add: Food image

--- src/main/java/com/btl_oop/Utils/DBConnection.java
+++ src/main/java/com/btl_oop/Utils/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DBConnection {
     public static Connection getConnection() throws SQLException
     {
-        String URL ="jdbc:mysql://127.0.0.1:3306/restaurant_management";
+        String URL ="jdbc:mysql://127.0.0.1:3306/restaurant_management2";
         String USER = "root";
         String PASSWORD ="123456";
         return DriverManager.getConnection(URL , USER, PASSWORD);
